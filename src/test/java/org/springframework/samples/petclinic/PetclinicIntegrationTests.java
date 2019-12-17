149e191c23fc5bc2a7daf92f693e300212cc17d6
Vishal M. Khot <vishal.khot@newgen.co.in>
2019-12-17 10:15:03 +0530

    Tested code modification for git commit

--- src/test/java/org/springframework/samples/petclinic/PetclinicIntegrationTests.java
+++ src/test/java/org/springframework/samples/petclinic/PetclinicIntegrationTests.java
@@ -20,7 +20,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.samples.petclinic.vet.VetRepository;
-
+/*vishal*/
 @SpringBootTest
 class PetclinicIntegrationTests {
 
